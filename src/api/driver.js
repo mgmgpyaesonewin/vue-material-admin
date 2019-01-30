98f04170f67dc3f5bba620a5698b5a1350140aae
pyaesone <pyaesone@softhatch.com>
2019-01-30 10:22:16 +0630

    API data for summary and driver added

---
+++ src/api/driver.js
@@ -0,0 +1,20 @@
+const driversStatusData = [
+  {
+    value: 10,
+    name: "Drivers On Duty"
+  },
+  {
+    value: 25,
+    name: "Assign"
+  },
+  {
+    value: 50,
+    name: "Idle"
+  },
+  {
+    value: 25,
+    name: "Sign up"
+  }
+];
+
+export { driversStatusData };
---
+++ src/api/summary.js
@@ -0,0 +1,32 @@
+const summaryData = [
+  {
+    value: 10,
+    name: "Total Orders"
+  },
+  {
+    value: 10,
+    name: "Ready For Collection"
+  },
+  {
+    value: 50,
+    name: "Accepted"
+  },
+  {
+    value: 35,
+    name: "Cooking"
+  },
+  {
+    value: 25,
+    name: "pending"
+  },
+  {
+    value: 25,
+    name: "En-Route"
+  },
+  {
+    value: 50,
+    name: "Completed"
+  }
+];
+
+export { summaryData };
