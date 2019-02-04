6b5f4262226480e021ab99ff4d3b60c25528ef99
pyaesone <pyaesone@softhatch.com>
2019-02-04 18:46:29 +0630

    Code reformat

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,32 @@
           <page-header v-if="$route.meta.breadcrumb"></page-header>
           <div class="page-wrapper">
             <router-view></router-view>
-          </div>   
-           <!-- App Footer -->
+          </div>
+          <!-- App Footer -->
           <v-footer height="auto" class="white pa-3 app--footer">
-            <span class="caption">isocked.com Design &copy; {{ new Date().getFullYear() }}</span>
+            <span class="caption">
+              isocked.com Design &copy; {{ new Date().getFullYear() }}
+            </span>
             <v-spacer></v-spacer>
-            <span class="caption mr-1"> Make With Love </span> <v-icon color="pink" small>favorite</v-icon>
+            <span class="caption mr-1"> Make With Love </span>
+            <v-icon color="pink" small>favorite</v-icon>
           </v-footer>
         </v-content>
         <!-- Go to top -->
         <app-fab></app-fab>
         <!-- theme setting -->
-        <v-btn small fab dark falt fixed top="top" right="right" class="setting-fab" color="red" @click="openThemeSettings">
+        <v-btn
+          small
+          fab
+          dark
+          falt
+          fixed
+          top="top"
+          right="right"
+          class="setting-fab"
+          color="red"
+          @click="openThemeSettings"
+        >
           <v-icon>settings</v-icon>
         </v-btn>
         <v-navigation-drawer
@@ -30,9 +44,9 @@
           v-model="rightDrawer"
           hide-overlay
           fixed
-          >
+        >
           <theme-settings></theme-settings>
-        </v-navigation-drawer>        
+        </v-navigation-drawer>
       </v-app>
     </template>
     <template v-else>
@@ -50,19 +64,19 @@
       v-model="snackbar.show"
     >
       {{ snackbar.text }}
-      <v-btn dark flat @click.native="snackbar.show = false" icon> 
+      <v-btn dark flat @click.native="snackbar.show = false" icon>
         <v-icon>close</v-icon>
       </v-btn>
     </v-snackbar>
   </div>
 </template>
 <script>
-import AppDrawer from '@/components/AppDrawer';
-import AppToolbar from '@/components/AppToolbar';
-import AppFab from '@/components/AppFab';
-import PageHeader from '@/components/PageHeader';
-import ThemeSettings from '@/components/ThemeSettings';
-import AppEvents from  './event';
+import AppDrawer from "@/components/AppDrawer";
+import AppToolbar from "@/components/AppToolbar";
+import AppFab from "@/components/AppFab";
+import PageHeader from "@/components/PageHeader";
+import ThemeSettings from "@/components/ThemeSettings";
+import AppEvents from "./event";
 export default {
   components: {
     AppDrawer,
@@ -76,37 +90,33 @@ export default {
     rightDrawer: false,
     snackbar: {
       show: false,
-      text: '',
-      color: '',
+      text: "",
+      color: ""
     }
   }),
 
-  computed: {
+  computed: {},
 
-  },
-
-  created () {
+  created() {
     AppEvents.forEach(item => {
       this.$on(item.name, item.callback);
     });
     window.getApp = this;
   },
   methods: {
-    openThemeSettings () {
+    openThemeSettings() {
       this.$vuetify.goTo(0);
-      this.rightDrawer = (!this.rightDrawer);
+      this.rightDrawer = !this.rightDrawer;
     }
-  },
-
+  }
 };
 </script>
 
 <style lang="stylus" scoped>
-  .setting-fab 
-    top:50%!important; 
-    right:0;
-    border-radius:0  
-  .page-wrapper
-    min-height:calc(100vh - 64px - 50px - 81px );  
-
+.setting-fab
+  top:50%!important;
+  right:0;
+  border-radius:0
+.page-wrapper
+  min-height:calc(100vh - 64px - 50px - 81px );
 </style>
